ee45684880f6518c40c39f76b274df6dcd7216f3
new010330 <rlarudtjs74a@naver.com>
2023-09-08 20:43:01 +0900

    filter등록 js 작업중 server 코드 완료

--- kids-mall/kids-mall/src/main/resources/static/filter/js/filter.js
+++ kids-mall/kids-mall/src/main/resources/static/filter/js/filter.js
@@ -75,78 +75,100 @@
 
 
 const genderButtons = document.querySelectorAll(".gender-button");
-  const ageRangeButtons = document.querySelectorAll(".age-range-button");
-  const styleButtons = document.querySelectorAll(".style-button");
-  const completeButton = document.querySelector(".complete-button");
-  const priceMinInput = document.querySelector(".price-min");
-  const priceMaxInput = document.querySelector(".price-max");
+const ageRangeButtons = document.querySelectorAll(".age-range-button");
+const styleButtons = document.querySelectorAll(".style-button");
+const completeButton = document.querySelector(".complete-button");
+const priceMinInput = document.querySelector(".price-min");
+const priceMaxInput = document.querySelector(".price-max");
 
-  let gender = [];
-  let age = [];
-  let kidStyle = [];
+let gender = [];
+let age = [];
+let kidStyle = [];
 
-  styleButtons.forEach((button, index) => {
+styleButtons.forEach((button, index) => {
     button.onclick = () => {
-      styleButtons[index].classList.toggle("active-color");
+        styleButtons[index].classList.toggle("active-color");
     };
-  });
+});
 
-  ageRangeButtons.forEach((button, index) => {
+ageRangeButtons.forEach((button, index) => {
     button.onclick = () => {
-      ageRangeButtons[index].classList.toggle("active-color");
+        ageRangeButtons[index].classList.toggle("active-color");
     };
-  });
+});
 
-  completeButton.onclick = () => {
+genderButtons.forEach((button, index) => {
+    button.onclick = () => {
+        genderButtons[index].classList.toggle("active-color");
+    };
+});
+
+const unsetCheckbox = document.querySelector(".free-price input[type=checkbox]");
+
+unsetCheckbox.onclick = () => {
+    if (unsetCheckbox.checked) {
+        // 설정안함 버튼이 체크되었을 때 input range와 값을 나타내는 div를 비활성화
+        console.log("비활성화");
+        priceMinInput.disabled = true;
+        priceMaxInput.disabled = true;
+    } else {
+        // 설정안함 버튼이 체크되지 않았을 때 input range와 값을 나타내는 div를 활성화
+        console.log("활성화");
+        priceMinInput.disabled = false;
+        priceMaxInput.disabled = false;
+    }
+};
+
+completeButton.onclick = () => {
     setData();
     // 여기서 선택 정보를 서버로 보내는 Ajax 요청을 수행할 수 있습니다.
     const data = {
-      user_flag: null,
-      gender: gender.join(","),
-      age: age.join(","),
-      kid_style: kidStyle.join(","),
-      price_min: priceMinInput.value,
-      price_max: priceMaxInput.value
+        user_flag: null,
+        gender: gender.join(","),
+        age: age.join(","),
+        kid_style: kidStyle.join(","),
+        price_min: priceMinInput.value,
+        price_max: priceMaxInput.value
     };
 
     // Ajax 요청 보내는 코드
     $.ajax({
-      async: false,
-      type: "post",
-      url: "/api/v1/search/filter",
-      data: data,
-      dataType: "json",
-      success: (response) => {
-        console.log(response.data);
-        // Ajax 요청 성공 시 실행되는 코드
-      },
-      error: (error) => {
-        console.log(error);
-        // Ajax 요청 오류 시 실행되는 코드
-      }
+        async: false,
+        type: "post",
+        url: "/api/v1/search/filter",
+        data: data,
+        dataType: "json",
+        success: (response) => {
+            console.log(response.data);
+            // Ajax 요청 성공 시 실행되는 코드
+        },
+        error: (error) => {
+            console.log(error);
+            // Ajax 요청 오류 시 실행되는 코드
+        }
     });
-  };
+};
 
-  function setData() {
+function setData() {
     gender = [];
     age = [];
     kidStyle = [];
 
     styleButtons.forEach((button, index) => {
-      if (button.classList.contains("active-color")) {
-        kidStyle.push(button.textContent);
-      }
+        if (button.classList.contains("active-color")) {
+            kidStyle.push(button.textContent);
+        }
     });
 
     ageRangeButtons.forEach((button, index) => {
-      if (button.classList.contains("active-color")) {
-        age.push(button.textContent);
-      }
+        if (button.classList.contains("active-color")) {
+            age.push(button.textContent);
+        }
     });
 
     genderButtons.forEach((button, index) => {
-      if (button.classList.contains("active-color")) {
-        gender.push(button.textContent);
-      }
+        if (button.classList.contains("active-color")) {
+            gender.push(button.textContent);
+        }
     });
-  }
+}
