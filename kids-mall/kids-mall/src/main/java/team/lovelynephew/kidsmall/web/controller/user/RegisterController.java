888e53b64b8c96d0c421819a75bb53a0ed059232
[yeonkyeong <kyk9725@naver.com>
2023-09-18 20:07:49 +0900

    html 및 css, 로그인, 회원가입 수정

--- kids-mall/kids-mall/src/main/java/team/lovelynephew/kidsmall/web/controller/user/RegisterController.java
+++ kids-mall/kids-mall/src/main/java/team/lovelynephew/kidsmall/web/controller/user/RegisterController.java
@@ -3,13 +3,20 @@ package team.lovelynephew.kidsmall.web.controller.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import team.lovelynephew.kidsmall.domain.user.RegisterEntity;
+import team.lovelynephew.kidsmall.service.user.RegisterService;
+import team.lovelynephew.kidsmall.web.dto.CMRespDto;
 import team.lovelynephew.kidsmall.web.dto.user.RegisterDto;
+
 
 @Controller
 public class RegisterController {
@@ -21,22 +28,19 @@ public class RegisterController {
 	
 	@GetMapping("/register") 
 	public String getRegister() {
-		return "employees/register2";
-	}
-	
-	@PostMapping("/register")
-	@ResponseBody
-	public Map<String, Object> saveRegister(@ModelAttribute RegisterDto registerDto) {
-		
-		Map<String, Object> map = new HashMap<>();
-		
-		
-		map.put("msg", "success");
-		
-		System.out.println(map);
-		
-		return map;
-		
+		return "employees/register";
 	}
 
+	@GetMapping("/find-id")
+	public String getFindId() {
+		
+		return "employees/findid";
+	}
+	
+	@GetMapping("/find-password")
+	public String getPassword() {
+		
+		return "employees/findpassword";
+	}
+	
 }
