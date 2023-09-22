d2189e8774e82a09b7ee4d374dbd3c843e4dd14f
MISO <>
2023-09-22 10:30:04 +0900

    찜하기 js연동 ing

--- kids-mall/kids-mall/src/main/resources/static/product/js/product.js
+++ kids-mall/kids-mall/src/main/resources/static/product/js/product.js
@@ -1,3 +1,4 @@
+
 //찜 하트 색상변경
     const heartButton = document.querySelector(".buy_bar_heart");
     const heartSvg = document.querySelector(".heart");
@@ -21,3 +22,41 @@ const buyModal = document.querySelector(".buy_modal_wrapper01");
 buyBtn.addEventListener('click', () => {
     buyModal.style.display = 'block';
 });
+
+
+//하트 누름 
+const wishIcon = document.querySelector(".buy_bar_heart");
+
+wishIcon.onclick = () => {
+    console.log("하트 버튼을 누른다. in js");
+    let wishListData = {
+        userId: "test_USERID",
+        prdCode: 3
+    }
+
+
+    $.ajax({
+        async: false,
+		type: "post",
+		url: "/wishList",
+		contentType: "application/json",
+		data: JSON.stringify(wishListData),
+		dataType: "json",
+		success: (response) => {
+			console.log(response.data);
+			if(response.data){
+				alert("회원가입 완료");
+				location.replace("/auth/signin");//뒤로가기 불가능 뒤로가기 눌렀응ㄹ때 defalut 주소로 감 (시큐리티설정/index)
+			}
+		},
+        error: (error) => {
+			if(error.status == 400) {//서버가 요청을 이해하지 못함 
+				alert(JSON.stringify(error.responseJSON.data));
+			}else {
+				console.log("요청실패");
+				console.log(error);
+			}
+		}
+
+    })
+}
