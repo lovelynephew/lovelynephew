3a73e1fadfad4b11315401c594bf3a433423688d
new010330 <rlarudtjs74a@naver.com>
2023-09-29 09:08:09 +0900

    페이징 처리 완료

--- kids-mall/kids-mall/src/main/resources/static/admin/js/order/order-list.js
+++ kids-mall/kids-mall/src/main/resources/static/admin/js/order/order-list.js
@@ -13,34 +13,95 @@ let call = 0;
 /**클릭한 페이지 버튼 number */
 let getPage = 1;
 
+let type = 0;
+
+// 한 화면에 보여질 페이지 그룹 크기
+let pageSize = 5;
+
+// 현재 페이지 그룹의 시작과 끝 계산
+let startPage = Math.floor((nowPage - 1) / pageSize) * pageSize + 1;
+let endPage = startPage + pageSize - 1;
+
 
 loadOrderListRequest(nowPage, searchValue);
+
+
+
+
+const firstButton = document.querySelector(".first-page");
+const preButton = document.querySelector(".pre-page");
+
+const nextButton = document.querySelector(".next-page");
+const lastButton = document.querySelector(".last-page");
 
 // 페이지 버튼 요소를 선택합니다.
 const pageButtons = document.querySelectorAll(".pages");
 
-// 각 페이지 버튼에 대한 클릭 이벤트.
-pageButtons.forEach((button, index) => {
-    button.onclick = () => {
-		getPage = index + 1;
-        // 클릭한 페이지 index와 현재 페이지가 다를 경우에만 동작
-		if(getPage != nowPage) {
-			console.log("클릭");
-			console.log(index);
-			nowPage = getPage;
-			loadOrderListRequest(getPage, searchValue);
-		}
-    };
-});
+preButton.onclick = () => {
+    console.log("이전 버튼 클릭");
+    type = 0;
+    getPage = nowPage - 1;
+    if (nowPage > 1) {
+        if (startPage > getPage) {
+            startPage = startPage - pageSize;
+            endPage = startPage + pageSize - 1;
+            type = 1;
+            loadOrderListRequest(getPage, searchValue);
 
+        }else {
+            loadOrderListRequest(getPage, searchValue);
+        }
+        nowPage = getPage;
+    }
+}
 
-function loadOrderListRequest(nowPage, searchValue) {
+nextButton.onclick = () => {
+    console.log("다음 버튼 클릭");
+    getPage = nowPage + 1;
+    type = 0;
+    if (nowPage < totalPage) {
+        if (getPage > endPage) {
+            type = 2;
+            loadOrderListRequest(getPage, searchValue);
+            startPage = Math.floor((getPage - 1) / pageSize) * pageSize + 1;
+            endPage = startPage + pageSize - 1;
+        }else {
+            loadOrderListRequest(getPage, searchValue);
+        }
+        nowPage = getPage;
+    }
+}
+
+firstButton.onclick = () => {
+    type = 0; // 타입 초기화
+    getPage = 1; // 첫 번째 페이지로 이동
+    startPage = 1; // 시작 페이지도 첫 번째 페이지로 설정
+    endPage = startPage + pageSize - 1; // 끝 페이지 업데이트
+    
+    loadOrderListRequest(getPage, searchValue);
+    nowPage == getPage;
+}
+
+lastButton.onclick = () => {
+    getPage = totalPage;
+    if(nowPage < totalPage) {
+        type = 4;
+        startPage = Math.floor((getPage - 1) / pageSize) * pageSize + 1;
+        endPage = totalPage;
+        loadOrderListRequest(getPage, searchValue);
+        console.log("lastButton클릭: " + startPage);
+        console.log(endPage);
+    }
+    nowPage = getPage;
+}
+
+function loadOrderListRequest(getPage, searchValue) {
     $.ajax({
         async: false,
         type: "get",
         url: "/admin/order/datalist",
         data: {
-            "page": nowPage,
+            "page": getPage,
             "searchValue": searchValue
         },
         dataType: "json",
@@ -63,7 +124,8 @@ function getOrderDataList(data) {
 	if(data != null) {
 		for(let i = 0; i < data.length; i++) {
 			console.log("getPage: " + getPage);
-            let no = ((getPage - 1) * 5 + 1) + i;
+            console.log("PAGE: " + getPage);
+            let no = ((getPage - 1) * 10 + 1) + i;
             console.log("no"+no);
 
             let price = priceToString(data[i].paymentAmount);
@@ -113,36 +175,88 @@ function getOrderDataList(data) {
 		`
 	}
 
-	//**처음 페이지 로딩 시에만 호출 */
-	if(call == 0) {
+	//처음 페이지 로딩 시에만 호출
+    if(call == 0 || type == 0) {
+        call = 0;
+        paging(data);
+    }else if(type != 0) {      
         paging(data);
     }
 
+    console.log("call: " + call);
     call++;
+
+    // 페이지 버튼 요소를 선택합니다.
+    const pageButtons = document.querySelectorAll(".pages");
+
+    // 각 페이지 버튼에 대한 클릭 이벤트.
+    pageButtons.forEach((button, index) => {
+        button.onclick = () => {
+            getPage = startPage + index;
+            console.log("page getPage: " + getPage);
+            // 클릭한 페이지 index와 현재 페이지가 다를 경우에만 동작
+            if(getPage != nowPage) {
+                type = 0;
+                console.log("클릭");
+                console.log(index);
+                loadOrderListRequest(getPage, searchValue);
+            }
+        };
+    });
+    nowPage = getPage;
 }
 
 
 //데이터 개수에 따른 페이지 개수
 function totalCnt(total) {
     //**한페이지 데이터 개수 */
-    let pageCount = 10
+    let pageCount = 10;
     totalPage = total % pageCount == 0 ? total / pageCount : Math.floor(total / pageCount) + 1;
     console.log("totalPage: " + totalPage);
+    console.log("total: " + total);
 }
 
 //데이터 개수에 맞는 페이지 버튼 넣기
 function paging(data) {
-    const page = document.querySelector(".page");
+    const page = document.querySelector(".page-number");
     totalCnt(data[0].totalCount);
-    
+    console.log("paging totalPage: " + totalPage);
+
     page.innerHTML = "";
 
-    for(let i = 1; i <= totalPage; i++) {    
-        page.innerHTML += `<strong class="pages">${i}</strong>`;
+    console.log("타입 이전");
+    console.log("type: " + type);
+    
+    for(let i = startPage; i <= endPage && i <= totalPage; i++) {
+        let prePage = (getPage - pageSize) + i;
+        let nextPage = i + pageSize;
+        let iPage = 
+        console.log("startPage: " + startPage);
+        if(call == 0) {
+            page.innerHTML += `<strong class="pages">${i}</strong>`;
+            console.log("첫 페이지 등록")
+
+        // 이전 버튼 클릭 시 이벤트
+        }else if(type == 1 && getPage != 1) {
+            page.innerHTML += `<strong class="pages">${prePage}</strong>`;
+            console.log("prePage: " + prePage);
+            
+        }else if(type == 2 && nextPage <= totalPage) {
+            page.innerHTML += `<strong class="pages">${nextPage}</strong>`;
+            console.log("nextPage: " + nextPage);
+            
+        }else if(type === 4 && nowPage < endPage) {
+            page.innerHTML += `<strong class="pages">${i}</strong>`;
+            console.log("i: " + i);
+        }
     }
+
+    
+    
 }
 
 // 숫자 쉼표 찍기
 function priceToString(price) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
+
