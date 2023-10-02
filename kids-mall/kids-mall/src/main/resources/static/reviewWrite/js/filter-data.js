cbce1345f02d04de876423d0401d6062d667dbd2
[yeonkyeong <kyk9725@naver.com>
2023-10-03 05:08:01 +0900

    리뷰작성페이지, 리뷰 상세페이지, 리뷰 작성가능한 페이지 html, css 작성 등

---
+++ kids-mall/kids-mall/src/main/resources/static/reviewWrite/js/filter-data.js
@@ -0,0 +1,61 @@
+function submit() {
+    setData();
+    // 여기서 선택 정보를 서버로 보내는 Ajax 요청을 수행할 수 있습니다.
+    console.log(kidStyle.join(","));
+
+    let data = {
+        user_flag: null,
+        gender: gender,
+        age: age,
+        kidStyle: kidStyle.join(","),
+        priceMin: priceMinInput.value,
+        priceMax: priceMaxInput.value
+    };
+
+    let getData = {
+        gender: gender,
+        age: age,
+        kidStyle: kidStyle.join(","),
+        priceMin: priceMinInput.value,
+        priceMax: priceMaxInput.value
+    }
+
+    $.ajax({
+        async: false,
+        type: "post",
+        url: "/api/v1/search/filter",
+        data: JSON.stringify(data),
+        contentType: 'application/json; charset=utf-8',
+        processData: false,
+        dataType: "json",
+        success: (response) => {
+            console.log(response.data);
+            alert("필터 등록 성공");
+            saveDataToSessionStorage(getData);
+            // console.log(priceMinInput.value);
+            // console.log(priceMaxInput.value);
+        },
+        error: (error) => {
+            console.log(error);
+        }
+    });
+    
+}
+
+function saveDataToSessionStorage(data) {
+    // 데이터를 JSON 문자열로 변환하여 sessionStorage에 저장
+    sessionStorage.setItem('cachedData', JSON.stringify(data));
+}
+
+function loadDataFromSessionStorage() {
+    // sessionStorage에서 데이터를 가져와서 JSON으로 파싱
+    const cachedData = sessionStorage.getItem('cachedData');
+    if (cachedData) {
+        return JSON.parse(cachedData);
+    }
+    return null; // 데이터가 없으면 null 반환
+}
+
+
+
+// submit();
